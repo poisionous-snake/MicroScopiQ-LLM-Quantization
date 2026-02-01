3c49901d574c5a0f323e8dee9c6ac6e98099208b
ysyx_23060217 <1972160722@qq.com>
2026-02-01 10:57:40 +0800

    Fix(critial error): specify groupsize

--- llm/opt.py
+++ llm/opt.py
@@ -105,7 +105,7 @@ def opt_sequential(model, dataloader, dev):
             print(i, name)
             print('Quantizing ...')
             gptq[name].fasterquant(
-                percdamp=args.percdamp, blocksize=args.blocksize, actorder=args.act_order, static_groups=args.static_groups, prunen=args.prunen, prunem=args.prunem
+                percdamp=args.percdamp, groupsize=args.groupsize, actorder=args.act_order, static_groups=args.static_groups, prunen=args.prunen, prunem=args.prunem
             )
             quantizers['model.decoder.layers.%d.%s' % (i, name)] = gptq[name].quantizer
             gptq[name].free()
